841feb6ff51a59526f31f9d14a57a93201b6d926
Nit <nitithon.taksaphan@gmail.com>
2026-01-05 12:59:16 +0700

    comment: complete mission #3 on Branch Mission 2 start already

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import ProductCategory from "./components/ProductCategory";
 import ProductHighlight from "./components/ProductHighlight";
 import Footer from "./components/Footer";
 
+// ทำรวบกับ Branch Mission #2 (Components & Props) แล้ว
+
 function App() {
   return (
     <>
